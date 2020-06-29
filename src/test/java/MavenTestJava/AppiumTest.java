c4dbbd6965fff6cb6f5042e87859d708f429390d
umesh <umeshkarkar22@gmail.com>
2020-06-30 01:03:57 +0530

    appiumTest

--- src/test/java/MavenTestJava/AppiumTest.java
+++ src/test/java/MavenTestJava/AppiumTest.java
@@ -8,11 +8,18 @@ public class AppiumTest {
 	public void AndriodApp(){
 		
 		System.out.println("AndriodApp");
+		System.out.println("AndriodApp");
+		System.out.println("AndriodApp");
 		
 	}
 	
 	@Test
 	public void IOSApp(){
+		System.out.println("IOSApp");
+	}
+
+	@Test
+	public void IOSApp1(){
 		System.out.println("IOSApp");
 	}
 
